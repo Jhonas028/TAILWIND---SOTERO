9266603c513b75dea249c09da3d4c11731325f80
Jhonas028 <soterojhonas028@gmail.com>
2026-03-02 15:49:34 +0800

    first commit

---
+++ js/theme-init.js
@@ -0,0 +1,10 @@
+// darkmode
+(function () {
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  }
+})();
